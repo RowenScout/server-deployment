2c67f86f3fa907613b145abfbbc446aed0240734
toastynerd <tylermorgan86@gmail.com>
2017-11-09 20:34:09 -0800

    change some stuff

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
 
 const server = require(__dirname + '/server');
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
+console.log('server started on port ' + process.env.PORT || 3000);
